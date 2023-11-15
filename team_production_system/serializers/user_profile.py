7f67b3ea425b851b7c13d9e1d124a2cc3ab3a00f
James Andrews <jbandrew14@gmail.com>
2023-11-15 11:52:03 -0500

    feat: added notification_settings field to UserProfileSerializer; sessions field does not appear in Response body

--- team_production_system/serializers/user_profile.py
+++ team_production_system/serializers/user_profile.py
@@ -2,7 +2,12 @@ from rest_framework import serializers
 
 from team_production_system.models import CustomUser
 
-from . import MenteeProfileSerializer, MentorProfileSerializer
+from . import (
+    MenteeProfileSerializer,
+    MentorProfileSerializer,
+    NotificationSettingsSerializer,
+    SessionSerializer,
+)
 
 
 # Version 2 of the serializer for the user's profile information
@@ -10,6 +15,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
     profile_photo = serializers.ImageField()
     mentor_info = MentorProfileSerializer(read_only=True, source='mentor')
     mentee_info = MenteeProfileSerializer(read_only=True, source='mentee')
+    notification_settings = NotificationSettingsSerializer(read_only=True)
+    sessions = SessionSerializer(many=True, read_only=True)
 
     class Meta:
         model = CustomUser
@@ -26,4 +33,6 @@ class UserProfileSerializer(serializers.ModelSerializer):
             'is_active',
             'mentee_info',
             'mentor_info',
+            'notification_settings',
+            'sessions',
         ]
